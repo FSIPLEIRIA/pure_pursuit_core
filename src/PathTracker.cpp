2f000dc1f91e56f315224694851f402b5238b058
carlostojal <carlos.tojal@hotmail.com>
2023-06-07 19:49:46 +0100

    fad-318 do some checks on the setters

--- src/PathTracker.cpp
+++ src/PathTracker.cpp
@@ -7,6 +7,9 @@
 
 namespace path_tracker {
     void path_tracker::PathTracker::setRate(int rate) {
+        if(rate <= 0)
+            throw std::runtime_error("An invalid rate was set!");
+
         this->rate = rate;
     }
 
@@ -15,6 +18,9 @@ namespace path_tracker {
     }
 
     void PathTracker::setPath(const lart_common::Path &p) {
+        if(p.getFullPath().size() <= 0)
+            throw std::runtime_error("A path of invalid size was given!");
+
         this->path = p;
         this->pathSet = true;
     }
